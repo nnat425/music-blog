fb1cdc6ca73cc895315f82c13bc6e95be7af15d0
Navraj Nat <nnat425@gmail.com>
2016-02-23 01:35:32 -0500

    Add code to connect to a database

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+<h1>My music blog</h1>
 
 <?php
 echo "Hello World!";
+?>
+
+<?php
+$servername = "localhost:8001";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    echo "Not connected";
+}
+echo "Connected successfully";
 ?>
 
 </body>
