51bd73136773c13504804d1dc5ffed8321cdb72c
Navraj Nat <nnat425@gmail.com>
2016-02-24 18:04:12 -0500

    Add form

--- index.php
+++ index.php
@@ -1,31 +1,54 @@
 <html>
 <body>
 
-<h1>My music blog</h1>
+  <h1>My music blog</h1>
 
-<?php
-echo "Hello World!";
-?>
+  <?php
+  echo "Hello World!";
+  ?>
 
-<?php
-$servername = "127.0.0.1";
-$username = "music_user";
-$password = "password";
-$dbname = "music_blog";
+  <?php
+  $servername = "127.0.0.1";
+  $username = "music_user";
+  $password = "password";
+  $dbname = "music_blog";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$conn) {
+  if (!$conn) {
     die("Connection failed: " . mysqli_connect_error() . "(" . mysqli_connect_errno() . ")" );
-}
-echo "Connected successfully";
-?>
+  }
+  echo "Connected successfully";
+  ?>
 
-<?php
-mysqli_close($conn);
-?>
+  <?php
+  mysqli_close($conn);
+  ?>
+
+
+  <form method="POST" action="index.php">
+    <br>
+    Name
+    <br>
+    <input type="text" name="title">
+    <br>
+    <br>
+    Email
+    <br>
+    <input type="text" name="link">
+    <br>
+    <br>
+    Description
+    <br>
+    <textarea rows="10" cols="50" maxlength="100" name="description">
+    </textarea>
+    <br>
+    <input type="submit" value="Send Message">
+  </form>
+
+
 
 
 
